083ecae4947e083a6d3a5cfb797ff7856a125469
mrcollier91 <colliermatt91@gmail.com>
2022-10-04 14:40:03 -0500

    submit

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { actors } from "../data";
 
+console.log(actors)
+
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      <div>
+        {actors.map(actor=>{
+          return(
+            <div key={actor.name}>
+              {actor.name}
+              <ul>movies</ul>
+              {actor.movies.map(movie=>{
+                return(
+                  <li key={movie}>{movie}</li>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Actors;
